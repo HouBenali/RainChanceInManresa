8730ad950f2c255667bc99565003c62e371dfc25
HoussamBenali <houssambenali@gmail.com>
2021-07-04 23:18:24 +0200

    Last edit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.test
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.test.databinding.ActivityMainBinding
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,15 +19,66 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val fechaView: TextView = findViewById(R.id.fecha) as TextView
+        val textView: TextView = findViewById(R.id.textView2) as TextView
+        val imageView : ImageView = findViewById(R.id.img1) as ImageView
 
-        val navView: BottomNavigationView = binding.navView
+        val client = OkHttpClient()
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val request = Request.Builder()
+            .url("https://api.weatherapi.com/v1/forecast.json?key=422c33389ec34450a3b131731210207&q=Manresa&days=1&aqi=no&alerts=no")
+            .build()
+
+        //val response : Response = client.newCall(request).execute()
+
+        // Coroutines not supported directly, use the basic Callback way:
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    println("----------------------------------------5------------------------------------")
+                    println(response)
+
+                    val resStr = response.body()!!.string()
+                    val json = JSONObject(resStr)
+
+                    val forecast = json.getJSONObject("forecast").getJSONArray("forecastday")[0]
+
+                    val StringDay = forecast.toString()
+                    val rain = JSONObject(StringDay).getJSONObject("day").get("daily_chance_of_rain").toString()
+                    val hoy = JSONObject(StringDay).get("date").toString()
+                    println(rain)
+
+                    this@MainActivity.runOnUiThread(java.lang.Runnable {
+                        fechaView.text = hoy
+
+                        if (rain.toInt() == 0){
+                            textView.text = "Hoy no llueve"
+                            imageView.setImageResource(R.drawable.sol)
+                        }
+                        else if (rain.toInt() > 0 && rain.toInt()<= 25){
+                            textView.text = "Poco probable"
+                            imageView.setImageResource(R.drawable.nubes)
+                        }
+                        else if (rain.toInt() > 25 && rain.toInt()< 75){
+                            imageView.setImageResource(R.drawable.lluvia)
+                            textView.text = "Hoy llueve"
+                        }
+                        else {
+                            imageView.setImageResource(R.drawable.tormenta)
+                            textView.text = "Mucha lluvia"
+
+                        }
+                    })
+                }
+            }
+        })
+
     }
+
+
 }
